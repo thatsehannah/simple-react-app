4fccd0c0493fafe0c5f42fc91c322acd564858d4
E. Hannah <echannah631@gmail.com>
2024-02-24 21:57:49 -0600

    created project page

---
+++ src/pages/ProductsPage.tsx
@@ -0,0 +1,21 @@
+import { products } from '../data/product';
+
+const ProductsPage = () => {
+  return (
+    <div className='text-center p-5'>
+      <h2 className='text-x1 font-bold text-slate-600'>
+        Here are some great tools for React
+      </h2>
+      <ul className='list-none m-0 p-0'>
+        {products.map((product) => (
+          <li
+            key={product.id}
+            className='p-1 text-base text-slate-800'
+          >
+            {product.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
