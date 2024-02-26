2a717fe98c53ae62aeb645efb6d6d6775196827b
E. Hannah <echannah631@gmail.com>
2024-02-26 07:14:21 -0600

    using nested routes and outlet component

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,15 +1,17 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { ProductsPage } from './pages/ProductsPage';
-import { Header } from './Header';
+import App from './App';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Header />,
-  },
-  {
-    path: 'products',
-    element: <ProductsPage />,
+    element: <App />,
+    children: [
+      {
+        path: 'products',
+        element: <ProductsPage />,
+      },
+    ],
   },
 ]);
 
