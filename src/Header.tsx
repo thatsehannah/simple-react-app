742fa5ba4a81438b2cf390ea3cd351e82a25ced8
E. Hannah <echannah631@gmail.com>
2024-02-27 08:19:21 -0600

    removed unneccessary testing, created search bar and query params

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,9 +1,31 @@
-import { NavLink, Link } from 'react-router-dom';
+import { FormEvent } from 'react';
+import { NavLink, Link, useSearchParams } from 'react-router-dom';
 import logo from './logo.svg';
 
 export const Header = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const handleSearchSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const search = formData.get('search') as string;
+    setSearchParams({ search });
+  };
+
   return (
     <header className='text-center text-slate-50 bg-slate-900 h-40 p-5'>
+      <form
+        className='relative text-right'
+        onSubmit={handleSearchSubmit}
+      >
+        <input
+          type='search'
+          name='search'
+          placeholder='Search'
+          defaultValue={searchParams.get('search') ?? ''}
+          className='absolute right-0 top-0 rounded py-2 px-3 text-gray-700'
+        />
+      </form>
       <Link to=''>
         <img
           src={logo}
--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,14 +1,28 @@
+import { Link, useSearchParams } from 'react-router-dom';
 import { products } from '../data/product';
-import { Link } from 'react-router-dom';
 
 export const ProductsPage = () => {
+  const [searchParams] = useSearchParams();
+
+  const getFilteredProducts = () => {
+    const search = searchParams.get('search');
+
+    if (search === null || search === '') {
+      return products;
+    }
+
+    return products.filter(
+      (product) => product.name.toLowerCase().indexOf(search.toLowerCase()) > -1
+    );
+  };
+
   return (
     <div className='text-center p-5'>
       <h2 className='text-x1 font-bold text-slate-600'>
         Here are some great tools for React
       </h2>
       <ul className='list-none m-0 p-0'>
-        {products.map((product) => (
+        {getFilteredProducts().map((product) => (
           <li key={product.id}>
             <Link
               to={`${product.id}`}
